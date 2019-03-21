7043b967b7de76f1c078b7587b07a294ca9cb609
khanishvili <khnshvl@gmail.com>
2019-03-21 11:01:23 -0400

    Images picture are moved to public state saved as string vice import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Preview from "./Preview";
-import Pic0 from "./Images/Kid.jpg";
-import Pic1 from "./Images/StarTrek.png";
-import Pic2 from "./Images/Taken.jpg";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      previewImage: Pic0
+      previewImage: "./Images/Kid.jpg"
     };
   }
   render() {
