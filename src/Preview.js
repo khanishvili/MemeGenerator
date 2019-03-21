e2cb5c4a4245495d590122bcd8fa89351ea69a6a
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-03-21 15:36:42 -0400

    Centered text on preview

--- src/Preview.js
+++ src/Preview.js
@@ -4,9 +4,16 @@ export default class Preview extends Component {
   render() {
     const { image, top, bottom } = this.props;
     return (
-      <div className="preview">
+      <div
+        className="preview"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center"
+        }}
+      >
         <h1 className="top-text">{top}</h1>
-        <img src={image} alt="Selected Meme Template" />
         <h1 className="bottom-text">{bottom}</h1>
       </div>
     );
