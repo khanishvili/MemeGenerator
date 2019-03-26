ea861eafd137bfbda53a13ef79bc0457f0096ebd
Alex Albright <alexanderjalbright@gmail.com>
2019-03-26 11:36:24 -0400

    Created api project and test project

---
+++ backend/MemeGenerator.Tests/MemesControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MemeGenerator.Tests
+{
+    public class MemesControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
