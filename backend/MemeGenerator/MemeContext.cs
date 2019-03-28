33a307b020a06ce7cf0daf84674a1df691d5704e
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-03-28 09:40:54 -0400

    Changed order of seed data

--- backend/MemeGenerator/MemeContext.cs
+++ backend/MemeGenerator/MemeContext.cs
@@ -28,45 +28,44 @@ namespace MemeGenerator
                     new Meme()
                     {
                         Id = 1,
-                        Top = "Js on my tree",
-                        Bottom = "Yo git like me",
-                        Image = "./Images/4.jpg"
+                        Top = "When you've been holding a fart in the whole interview,",
+                        Bottom = "and you feel a sneeze coming",
+                        Image = "./Images/2.jpg"
                     },
                     new Meme()
                     {
                         Id = 2,
-
-                        Top = "I will look for you and I will find you",
-                        Bottom = "\"Cross-Origin Request Blocked\"",
-                        Image = "./Images/Taken.jpg"
-                    },
-                    new Meme()
-                    {
-                        Id = 3,
-                        Top = "International Man",
-                        Bottom = "of MVC",
-                        Image = "./Images/9.jpg"
-                    },
-                    new Meme()
-                    {
-                        Id = 4,
-                        Top = "When a new test passes",
-                        Bottom = "but old ones start failing",
-                        Image = "./Images/5.jpg"
-                    },
-                    new Meme()
-                    {
-                        Id = 5,
                         Top = "WHY THE FUNCTION",
                         Bottom = "is it called a method and an action?",
                         Image = "./Images/StarTrek.png"
                     },
                     new Meme()
                     {
+                        Id = 3,
+                        Top = "When a new test passes",
+                        Bottom = "but old ones start failing",
+                        Image = "./Images/5.jpg"
+                    },
+                    new Meme()
+                    {
+                        Id = 4,
+                        Top = "International Man",
+                        Bottom = "of MVC",
+                        Image = "./Images/9.jpg"
+                    },
+                    new Meme()
+                    {
+                        Id = 5,
+                        Top = "I will look for you and I will find you",
+                        Bottom = "\"Cross-Origin Request Blocked\"",
+                        Image = "./Images/Taken.jpg"
+                    },
+                    new Meme()
+                    {
                         Id = 6,
-                        Top = "When you've been holding a fart in the whole interview,",
-                        Bottom = "and you feel a sneeze coming",
-                        Image = "./Images/2.jpg"
+                        Top = "Js on my tree",
+                        Bottom = "Yo git like me",
+                        Image = "./Images/4.jpg"
                     }
                 );
 
--- backend/MemeGenerator/Migrations/20190328133852_changedOrder.Designer.cs
+++ backend/MemeGenerator/Migrations/20190328133852_changedOrder.Designer.cs
@@ -3,14 +3,16 @@ using MemeGenerator;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MemeGenerator.Migrations
 {
     [DbContext(typeof(MemeContext))]
-    partial class MemeContextModelSnapshot : ModelSnapshot
+    [Migration("20190328133852_changedOrder")]
+    partial class changedOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
